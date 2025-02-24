5ca274f602a2385ddbcbb0307d91065701ad9250
wallace0003 <161092400+wallace0003@users.noreply.github.com>
2025-02-24 22:25:23 +0000

    fazendo o remover

--- aulas/aula03.c
+++ aulas/aula03.c
@@ -54,6 +54,43 @@ void inserir(LDDE *lista,   int valor){
     lista->qtd++;
 }
 
+void remover(LDDE *lista, int valor){
+    Celula *atual = lista->primeiro;
+
+    // Procurar o valor na lista
+    while(atual != NULL && atual->valor != valor){
+        atual = atual->proximo;
+    }
+
+    // Caso o valor não tenha sido encontrado
+    if(atual == NULL){
+        printf("Valor %d não encontrado na lista.\n", valor);
+        return;
+    }
+
+    // Se for o primeiro elemento
+    if(atual->anterior == NULL){
+        lista->primeiro = atual->proximo;
+        if(atual->proximo != NULL){
+            atual->proximo->anterior = NULL;
+        }
+    }
+    // Se for o último elemento
+    else if(atual->proximo == NULL){
+        atual->anterior->proximo = NULL;
+    }
+    // Se for um elemento no meio
+    else{
+        atual->anterior->proximo = atual->proximo;
+        atual->proximo->anterior = atual->anterior;
+    }
+
+
+    free(atual);
+    lista->qtd--;
+}
+
+
 void mostrar(LDDE *lista){
     Celula *atual = lista->primeiro;
     while(atual != NULL){
